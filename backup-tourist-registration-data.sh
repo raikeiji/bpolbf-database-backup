c0f6089ebf21979bf82543cd3f9f843708bee5c5
Engelbertus Vione <raikeiji@gmail.com>
2022-12-17 08:28:46 +0700

    backup website databasescript

--- backup-tourist-registration-data.sh
+++ backup-tourist-registration-data.sh
@@ -1,5 +1,8 @@
 #!/bin/sh -e
 
 location=~/systemdatabackup/tourism-online-registration/`date +%Y%m%d_%H%M%S`.sql
+location2=~/systemdatabackup/website/`date +%Y%m%d_%H%M%S`.sql
 
 mysqldump --user=root --password=admin@B4j0 db_online-booking > $location -v
+
+mysqldump --user=root --password=@B4j0_us3r! db_web > $location2 -v
